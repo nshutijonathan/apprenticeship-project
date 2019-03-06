4c89df99a727a127380d0d8748af0ff9a8b389c9
okellogabrielinnocent <okellogabrielinnocent@gmail.com>
2019-03-06 10:52:43 +0300

    chore(setup):setup django project - change admin text to HealthID Admin on admin portal - change url to path in url.py file [164404495]

---
+++ healthid/schema.py
@@ -0,0 +1,17 @@
+import graphene
+
+"""
+Example of usage
+import examples.schema
+
+
+class Query(examples.schema.Query, graphene.ObjectType):
+    pass
+
+
+class Mutation(examples.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+"""
---
+++ healthid/urls.py
@@ -0,0 +1,29 @@
+"""healthid URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
+
+admin.site.site_header = "HealthID Admin"
+admin.site.site_title = "HealthID Admin"
+admin.site.index_title = "Welcome to HealthID"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+]
